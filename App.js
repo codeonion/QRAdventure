9192a3fe7c7e4aa7bae81a5205be30c41429f3f4
Muhammad Ali <muhammad.ali@sdilabsinc.com>
2021-06-30 15:55:32 +0500

    First commit setup

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
 
 export default function App() {
+  async function registerForPushNotificaiton(){
+    const {status} = await Permissions.getAsync(Permissions.Notifications);
+    if(status != "granted"){const {status} = await Permissions.getAsync(Permissions.Notifications);}
+    if(status != "granted"){alert("Gailed to get the  push token");return;}
+
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
