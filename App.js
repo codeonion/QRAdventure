c737ded7cde3200189a27032dc52ad12764c6bd2
Muhammad Ali <muhammad.ali@sdilabsinc.com>
2021-06-30 16:58:04 +0500

    Header complete, with props and default props and styling

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 const App = () => {
   return (
     <View style={styles.contianer}>
-      <Header />
+      <Header title='Shopping List' />
     </View>
   );
 }
