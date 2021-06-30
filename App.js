3822e2182ca47d7fed86ea1e8ade3f9f6687232c
Muhammad Ali <muhammad.ali@sdilabsinc.com>
2021-06-30 18:05:28 +0500

    UUID updated to latest and state based list shown

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import React, {useState} from 'react';
+import { View, Text, StyleSheet,FlatList } from 'react-native'
 import Header from './components/Header';
+import 'react-native-get-random-values';
+import { v4 as uuidv4 } from 'uuid';
+
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuidv4(), text: 'Milk'},
+    {id: uuidv4(), text: 'Potato'},
+    {id: uuidv4(), text: 'Tomato'},
+    {id: uuidv4(), text: 'Juice'},
+    {id: uuidv4(), text: 'Bread'},
+  ]);
   return (
     <View style={styles.contianer}>
       <Header title='Shopping List' />
+      <FlatList data={items} renderItem={({item}) => (
+        <Text>{item.text}</Text>
+      )} />
     </View>
   );
 }
