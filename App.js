a741e2c8008157aee538215c851d5a051165f405
Muhammad Ali <muhammad.ali@sdilabsinc.com>
2021-06-30 16:31:27 +0500

    Separate CSS implemented

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const App = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{color:'darkslateblue', fontSize: 30}}>Hello World</Text>
+    <View style={styles.contianer}>
+      <Text style={styles.text}>Hello World</Text>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  contianer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {color:'darkslateblue', fontSize: 30},
+})
 export default App;
