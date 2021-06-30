00c818669811afd6607be4b5cf32ec5d2632ef29
Muhammad Ali <muhammad.ali@sdilabsinc.com>
2021-06-30 16:26:40 +0500

    displaying hello world

--- App.js
+++ App.js
@@ -1,31 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import * as Notifications from 'expo-notifications';
+import { View, Text } from 'react-native'
 
-export default function App() {
-  async function registerForPushNotificaiton(){
-    const {status} = await Permissions.getAsync(Permissions.Notifications);
-    if(status != "granted"){const {status} = await Permissions.getAsync(Permissions.Notifications);}
-    if(status != "granted"){alert("Gailed to get the  push token");return;}
-
-    token = (await Notifications.getExpoPushTokenAsync()).data;
-
-  }
-
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View}>
+      <Text>Hello World</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
