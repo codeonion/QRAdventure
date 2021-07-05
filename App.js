1edfcf59eba1989fee1063e05df374731c91f7a9
Muhammad Ali <muhammad.ali@sdilabsinc.com>
2021-07-05 08:33:06 +0500

    Little QR Scanner App

--- App.js
+++ App.js
@@ -1,48 +1,83 @@
-import React, {useState} from 'react';
-import { View, Text, StyleSheet, FlatList, Alert } from 'react-native'
+import React, {useState, useEffect} from 'react';
+import { View, Text, StyleSheet, FlatList, Alert, Button  } from 'react-native'
 import 'react-native-get-random-values';
 import { v4 as uuidv4 } from 'uuid';
 
+/* Custom Components */
 import Header from './components/Header';
 import ListItem from './components/ListItem'
 import AddItem from './components/AddItem'
 
+/* 3rd Party Components */
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
 // https://youtu.be/Hf4MJH0jDb4?t=2188 n
 const App = () => {
-  const [items, setItems] = useState([
-    {id: uuidv4(), text: 'Milk'},
-    {id: uuidv4(), text: 'Potato'},
-    {id: uuidv4(), text: 'Tomato'},
-    {id: uuidv4(), text: 'Juice'},
-    {id: uuidv4(), text: 'Bread'},
-  ]);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
 
-  const deleteItem = (id) => {
-    setItems(prevItems => {
-      return prevItems.filter(item => item.id != id);
-    });
-  }
-  
-  const addItem = (text) => {
-    if (!text) {
-      Alert.alert("Error", "Cannot add empty records",[{text: "Ok"}]);
-    } else {
-      
-      setItems(prevItems => {
-        return [{id: uuidv4(), text}, ...prevItems];
-      });
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  var dict = {
+    "apple": "You have an Apple"
+  };
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    console.log(typeof(dict[data]));
+    if(dict[data] != undefined){
+      alert(dict[data]);
+      return <h1>daw</h1>
+    }else{
+      alert(`Bar code with type ${type} and data ${data} has been scanned!`);
     }
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
   }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+
+  // const [items, setItems] = useState([
+  //   {id: uuidv4(), text: 'Milk'},
+  //   {id: uuidv4(), text: 'Potato'},
+  //   {id: uuidv4(), text: 'Tomato'},
+  //   {id: uuidv4(), text: 'Juice'},
+  //   {id: uuidv4(), text: 'Bread'},
+  // ]);
+
+  // const deleteItem = (id) => {
+  //   setItems(prevItems => {
+  //     return prevItems.filter(item => item.id != id);
+  //   });
+  // }
+  
+  // const addItem = (text) => {
+  //   if (!text) {
+  //     Alert.alert("Error", "Cannot add empty records",[{text: "Ok"}]);
+  //   } else {
+      
+  //     setItems(prevItems => {
+  //       return [{id: uuidv4(), text}, ...prevItems];
+  //     });
+  //   }
+  // }
   return (
     <View style={styles.contianer}>
-      <Header title='Shopping List' />
-      <AddItem addItem={addItem} />
-      <FlatList 
-        data={items} 
-        renderItem={({item}) => (
-          <ListItem item={item} deleteItem={deleteItem} />
-        )}  
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
       />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+      
     </View>
   );
 }
