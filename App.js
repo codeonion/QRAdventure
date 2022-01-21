e03efc17863978207a8fe36afe2decc278836e2c
Muhammad Ali <mycodeonion@gmail.com>
2022-01-21 18:59:37 +0500

    QR Codes also show linked to name
    added images, qr control improved.

--- App.js
+++ App.js
@@ -68,34 +68,37 @@ const App = () => {
     "showtext_template": {"type": "modal_text", "data_text": "Bro, I am showing right", "qr_data_uri": null},
     "showimage_template": {"type": "modal_image", "data_text": "Bro, I am showing right", "qr_data_uri_image": "ali-cat.jpeg"},
     "show_modal_image_audio_template": {"type": "modal_image_text_audio", "data_text": "Bro, I am showing right", "qr_data_uri_image": "ali-cat.jpeg", "qr_data_uri_audio": "00-alibhai.mp3"},
+    
+    
     // "alphaA": {"type": "text", "data": '00-alibhai.mp3'}
     /* Animal Sounds */
     "01-cat_meow.mp3": {"type": "play_audio", "data_text": "", "qr_data_uri_audio": '01-cat_meow.mp3'},
 
     /* Adventure Series */
-    "adventure-01": {"type": "modal_image", "data_text": "A01: Give in hand. Leads Controller Drawer.", "qr_data_uri_image": 'adventure-01.jpg'},
-    "adventure-02": {"type": "modal_image", "data_text": "A02: Sitting Room, On Dining Table", "qr_data_uri_image": 'adventure-02.jpg'},
-    "adventure-03": {"type": "modal_image", "data_text": "A03: In a plant pot", "qr_data_uri_image": 'adventure-03.jpg'},
-    "adventure-04": {"type": "modal_image", "data_text": "A04: In Papa bag", "qr_data_uri_image": 'adventure-04.jpg'},
-    "adventure-05": {"type": "modal_image", "data_text": "A05: In Dressing Table Drawer", "qr_data_uri_image": 'adventure-05.jpg'},
-    "adventure-06": {"type": "modal_image", "data_text": "A06: In Fridge", "qr_data_uri_image": 'adventure-06.jpg'},
-    "adventure-07": {"type": "modal_image", "data_text": "A07: Below Carpet corner in Lounge", "qr_data_uri_image": 'adventure-07.jpg'},
-    "adventure-08": {"type": "modal_image", "data_text": "A08: Inside Dastarkhawan", "qr_data_uri_image": 'adventure-08.jpg'},
-    "adventure-09": {"type": "modal_image", "data_text": "A09: In a pillow", "qr_data_uri_image": 'adventure-09.jpg'},
-    "adventure-10": {"type": "modal_image", "data_text": "A10: In Masala Drawer Kitchen", "qr_data_uri_image": 'adventure-10.jpg'},
-    "adventure-11": {"type": "modal_image", "data_text": "A11: In Ayeza Clothes Cupboard", "qr_data_uri_image": 'adventure-11.jpg'},
-    "adventure-12": {"type": "modal_image", "data_text": "A12: Under Sitting Room Sofa", "qr_data_uri_image": 'adventure-12.jpg'},
-    "adventure-13": {"type": "modal_image", "data_text": "A13: With Dada", "qr_data_uri_image": 'adventure-13.jpg'},
-    "adventure-14": {"type": "modal_image", "data_text": "A14: With Mam", "qr_data_uri_image": 'adventure-14.jpg'},
-    "adventure-15": {"type": "modal_image", "data_text": "A15: With Mama", "qr_data_uri_image": 'adventure-15.jpg'},
-    "adventure-16": {"type": "modal_image", "data_text": "A16: With Papa", "qr_data_uri_image": 'adventure-16.jpg'},
-    "adventure-17": {"type": "modal_image", "data_text": "A17: In Freezer", "qr_data_uri_image": 'adventure-17.jpg'},
-    "adventure-18": {"type": "modal_image", "data_text": "A18: After Mumma Ki Baat manni pare gi", "qr_data_uri_image": 'task.png'},
-    "adventure-19": {"type": "modal_image", "data_text": "A19: After Papa ki baat mann ni paregi", "qr_data_uri_image": 'task.png'},
-    "adventure-20": {"type": "modal_image", "data_text": "A20: Mam ko Pehla aur dusra kalma sunana hoga", "qr_data_uri_image": 'task.png'},
-    "adventure-21": {"type": "modal_image", "data_text": "A21: Inside Sewing Machine Drawer", "qr_data_uri_image": 'adventure-21.jpg'},
-    "adventure-22": {"type": "modal_image", "data_text": "A22: In Bicycle Bucket", "qr_data_uri_image": 'adventure-22.jpg'},
-    "adventure-23": {"type": "play_audio", "data_text": "A23: Ali Bhai!", "qr_data_uri_audio": '00-alibhai.mp3'},
+    "adventure-01": {"type": "modal_image", "qr_data_uri_image": 'adventure-01',  "data_text": "A01 (Hand): Give in hand. Next one in Papa Drawer.", },
+    "adventure-02": {"type": "modal_image", "qr_data_uri_image": 'adventure-02',  "data_text": "A02 (Papa Drawer): Next in Sitting Room, On Dining Table", },
+    "adventure-03": {"type": "modal_image", "qr_data_uri_image": 'adventure-03',  "data_text": "A03 (On Dining Table): Next in a plant pot", },
+    "adventure-04": {"type": "modal_image", "qr_data_uri_image": 'adventure-04',  "data_text": "A04 (in a plant pot): Next in Papa bag", },
+    "adventure-05": {"type": "modal_image", "qr_data_uri_image": 'adventure-05',  "data_text": "A05 (in Papa bag): Next in Dressing Table Drawer", },
+    "adventure-06": {"type": "modal_image", "qr_data_uri_image": 'adventure-06',  "data_text": "A06 (in Dressing Table Drawer): Next in Toys Box White Large", },
+    "adventure-07": {"type": "modal_image", "qr_data_uri_image": 'adventure-07',  "data_text": "A07 (In Toy Box): Next Below Carpet corner in Lounge", },
+    "adventure-08": {"type": "modal_image", "qr_data_uri_image": 'adventure-08',  "data_text": "A08 (Under Lounge Carpet): Next Inside Dastarkhawan", },
+    "adventure-09": {"type": "modal_image", "qr_data_uri_image": 'adventure-09',  "data_text": "A09 (Inside Dastarkhawan): Next in a pillow", },
+    "adventure-10": {"type": "modal_image", "qr_data_uri_image": 'adventure-10',  "data_text": "A10 (In Pillow): Next in Masala Drawer Kitchen", },
+    "adventure-11": {"type": "modal_image", "qr_data_uri_image": 'adventure-11',  "data_text": "A11 (In Kitchen Drawer): Next in Ayeza Clothes Cupboard", },
+    "adventure-12": {"type": "modal_image", "qr_data_uri_image": 'adventure-12',  "data_text": "A12 (In Ayeza Clothes Cupboard): Next Under Sitting Room Sofa", },
+    "adventure-13": {"type": "modal_image", "qr_data_uri_image": 'task.png'    ,  "data_text": "A13 (Under Sofa): Next After Task", },
+    "adventure-14": {"type": "modal_image", "qr_data_uri_image": 'task.png'    ,  "data_text": "A14 (Task): Next After Task", },
+    "adventure-15": {"type": "modal_image", "qr_data_uri_image": 'task.png'    ,  "data_text": "A15 (Task): Next With Mama", },
+    "adventure-16": {"type": "modal_image", "qr_data_uri_image": 'task.png'    ,  "data_text": "A16 (With Mama): Next in Hello Kitty House", },
+    // "adventure-17": {"type": "modal_image", "qr_data_uri_image": 'adventure-17',  "data_text": "A17 (With Papa): Next Inside Helo Kitty House", },
+    "adventure-18": {"type": "modal_image", "qr_data_uri_image": 'task.png'    ,  "data_text": "A18 (Near Hello Kitty House): Next After Mumma Ki Baat manni pare gi", },
+    "adventure-19": {"type": "modal_image", "qr_data_uri_image": 'task.png'    ,  "data_text": "A19 (Mama Ki bat mano): Next After Papa ki baat manni paregi", },
+    "adventure-20": {"type": "modal_image", "qr_data_uri_image": 'task.png'    ,  "data_text": "A20 (Papa Ki Baat Mano): Next After Task", },
+    "adventure-21": {"type": "modal_image", "qr_data_uri_image": 'adventure-21',  "data_text": "A21 (Task): Inside Sewing Machine Drawer", },
+    "adventure-22": {"type": "modal_image", "qr_data_uri_image": 'adventure-22',  "data_text": "A22 (Sewing Machine): In Bicycle Bucket", },
+    "adventure-23": {"type": "modal_image", "qr_data_uri_image": 'adventure-23',  "data_text": "A23 (In Bicycle): In another flower pot", },
+    "adventure-24": {"type": "play_audio", "data_text": "A24: Ali Bhai!", "qr_data_uri_audio": '00-alibhai.mp3'},
   };  
   
   const handleBarCodeScanned = ({ type, data }) => {
@@ -103,6 +106,7 @@ const App = () => {
     if(dict[data] != undefined){ // Item Exists
       var qr_data = dict[data];
       showToastWithGravity(qr_data.type)
+      console.log(qr_data["qr_data_uri_image"])
       switch (qr_data.type) {
         case "play_audio": // Plain Audio Playback
           showToastWithGravity(`Playing Audio ${qr_data["qr_data_uri_audio"]}`)
@@ -116,9 +120,10 @@ const App = () => {
           setModalText(qr_data.data_text);
           break;
         case "modal_image": // Plain Text and Image Display
-          setModalImage(Images[qr_data["qr_data_uri_image"]])
-          setModalText(qr_data.data_text);
+          showToastWithGravity(qr_data["qr_data_uri_image"])
           setModalVisible(true);
+          setModalText(qr_data.data_text);
+          setModalImage(Images[qr_data["qr_data_uri_image"]])
           break;
         case "modal_image_text_audio": // Plain Text and Image Display and audio playback
           playSound(qr_data["qr_data_uri_audio"]);
@@ -162,7 +167,7 @@ const App = () => {
             }}>
             <View style={styles.centeredView}>
               <View style={styles.modalView}>
-                <Image source={ModalImage} style={{  }} /> 
+                <Image source={ModalImage} style={styles.modalImage} /> 
                 <Text style={styles.modalText}>{modalText}</Text>
     
                 <TouchableHighlight
@@ -228,8 +233,13 @@ const styles = StyleSheet.create({
   },
   modalText: {
     marginBottom: 15,
-    fontSize: 40,
+    fontSize: 20,
     textAlign: 'center',
+  },
+  modalImage: {
+    width: '100%',
+    height: undefined,
+    aspectRatio: 2,
   },
 });
 
